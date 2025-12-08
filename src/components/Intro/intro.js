3d823e534fb6c435154cb75b8c0f0e7782b9efd7
Pulkit Dubey <46dubey@gmail.com>
2025-12-08 20:55:18 +0530

    Fixing errors in intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,49 +6,49 @@ import btnImg from '../../assets/hireme.png';
 import { Link } from 'react-scroll';
 
 const Intro = () => {
-    const typedRef = useRef(null);
+  const typedRef = useRef(null);
 
-    useEffect(() => {
-        const typed = new Typed(typedRef.current, {
-            strings: ['Web Developer','Tech Enthusiast', 'BCA Graduate', 'Educator'],
-            typeSpeed: 50,
-            backSpeed: 30,
-            backDelay: 1800,
-            loop: true
-        });
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: ['Web Developer', 'Tech Enthusiast', 'BCA Graduate', 'Educator'],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1800,
+      loop: true,
+    });
 
-        return () => {
-            typed.destroy();
-        };
-    }, []);
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
-    return (
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hello,</span>
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
 
-                <span className="introText">
-                    I'm <span className="introName">Pulkit Dubey</span> <br />
-                    <span className="role">
-                        <span ref={typedRef}></span>
-                    </span>
-                </span>
+        <span className="introText">
+          I'm <span className="introName">Pulkit Dubey</span> <br />
+          <span className="role">
+            <span ref={typedRef}></span>
+          </span>
+        </span>
 
-                <p className="introPara">
-                    I am a skilled web designer with experience in creating <br />
-                    visually appealing and user friendly websites.
-                </p>
+        <p className="introPara">
+          I am a skilled web designer with experience in creating
+          visually appealing and user friendly websites.
+        </p>
 
-                <Link>
-                    <button className="btn hireBtn">
-                        <img src={btnImg} alt="Hire" className='btnImg'/> Hire Me
-                    </button>
-                </Link>
-            </div>
+        <Link to="contact" smooth={true} duration={500}>
+          <button className="btn hireBtn">
+            <img src={btnImg} alt="Hire" className="btnImg" /> Hire Me
+          </button>
+        </Link>
+      </div>
 
-            <img src={bg} alt="Profile" className="bg" />
-        </section>
-    );
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
 };
 
 export default Intro;
